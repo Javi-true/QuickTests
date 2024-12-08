043a6245c3d9dc614dbfc388f66978c7ea81c746
Javier Trujillo <rivera.dev@outlook.com>
2024-12-08 05:19:43 -0600

    String methods and Wrapper classes

---
+++ src/Smethods.java
@@ -0,0 +1,9 @@
+public class Smethods {
+
+    public static void main(String[] args){
+
+        String text = "Hola mi nombre es Javier";
+
+        System.out.println(text.replaceAll("\\s", ""));
+    }
+}
---
+++ src/Wrap.java
@@ -0,0 +1,12 @@
+public class Wrap {
+
+    public static void main(String[] args){
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.12;
+        String e = "Javi";
+
+
+    }
+}
