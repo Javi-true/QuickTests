7923d6a6dc54bb7d548708add461a82a76d53b12
Javier Trujillo <rivera.dev@outlook.com>
2024-12-10 17:15:55 -0600

    Ejemplo generacion numeros aleatorios, con Math y con Clase Random

--- src/RandomNum.java
+++ src/RandomNum.java
@@ -1,6 +1,16 @@
+import java.util.Random;
 public class RandomNum {
     public static void main(String[] args){
         //primer metodo.
+        double randomDouble = Math.random() * 10;
+        int intCasting = (int) randomDouble + 1;
 
+        //System.out.print(intCasting);
+
+        //segundo metodo, clase Random.
+        Random random1 = new Random();
+
+        int intRandom = random1.nextInt(10) + 1;
+        System.out.print(intRandom);
     }
 }
