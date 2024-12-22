ae6a29b3868102589f9e7073871303243356e7b7
Javier Trujillo <rivera.dev@outlook.com>
2024-12-22 14:01:13 -0600

    final

--- src/Animals.java
+++ src/Animals.java
@@ -5,32 +5,23 @@ public class Animals {
     char gender;
 
     //constructor .
-    public Animals(String name, int age, char gender){
+    public Animals(String name, int age, char gender) {
         this.name = name;
         this.age = age;
         this.gender = gender;
     }
 
     //toString().
-    public String toString(){
-        return "Animals{name = "+name+", age = "+age+", gender = "+gender+"}";
+    public String toString() {
+        return "Animals{name = " + name + ", age = " + age + ", gender = " + gender + "}";
     }
 
     //sound method.
-    public void Sound(){
+    public void Sound() {
         System.out.println("Animals make sounds");
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-<<<<<<< HEAD
-        Animals perro = new Perros("Perrito", 10,'M',"Pug");
-        //System.out.println();
-        perro.Sound();
-=======
-        Perros perro1 = new Perros("Juanito", 1, 'M', "Pug");
-        System.out.println(perro1);
-        perro1.Sound();
->>>>>>> 089d58a167ab4dcd99ccfedf848a464677d77e5c
     }
 }
